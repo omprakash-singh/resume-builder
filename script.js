a8f6b7c8eb47ec8c4f40af81880792b017a2cd35
omprakash-singh <omprakashingh11009@gmail.com>
2022-05-04 20:58:42 +0530

    template

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const hidden = document.querySelector('.navbar');
 menuButton.addEventListener('click', () => {
      hidden.classList.toggle('hidden');
 })
+
+var swiper = new Swiper(".mySwiper", {
+     slidesPerView: 1,
+     spaceBetween: 30,
+     loop: true,
+     pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+     },
+     navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+     },
+});
